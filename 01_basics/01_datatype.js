fc642e8e0e3ea80f9e7c37281dfe300aa57ef0d5
MuhammadBurhan1 <128923339+MuhammadBurhan1@users.noreply.github.com>
2025-06-01 06:55:20 +0000

    Done with primitive datatype, a little talk about non-primitive datatype

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,18 @@
+"use strict"
+// alert ("2" + 3)  There is a different syntax of alert in node.js
+let name ="Burhan"
+let id = 18;
+let isloggedoff = false
+// number=> 2 to the power of 53
+// bigint
+// string=> " "
+// Boolean=> true/false
+// Null=> which mean there is no value or standalone value or it is the representation of an empty value
+// Undefined=>
+// Symbol=> Uniquesness 
+//All above are primitive datatype , now i will write about non-primitive datatype
+//Object
+console.log(typeof null)
+console.log(typeof undefined)
+
+
