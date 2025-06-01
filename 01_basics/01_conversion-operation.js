590da4f07ff1bc2e0f14414526678a7c1d80c29e
MuhammadBurhan1 <128923339+MuhammadBurhan1@users.noreply.github.com>
2025-06-01 07:50:51 +0000

    updated

---
+++ 01_basics/01_conversion-operation.js
@@ -0,0 +1,34 @@
+"use strict" // Treat all JS code as newer version
+// let score = "Burhan"
+// console.log(typeof score)
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// // This is when we are trying to convert into a number
+// // "33"=> 33
+// // "33abc"=> NaN
+// // null=> 0
+// // undefined=> NaN
+// let isLoggedIn = false;
+// let inNumber = Number(isLoggedIn)
+// console.log(typeof inNumber)
+// console.log(inNumber)
+// //So, here we are trying to convert boolean into number
+// //So, when true => 1, false=> 0
+// isLoggedIn = ""
+// let inBoolean = Boolean(isLoggedIn)
+// console.log(inBoolean)
+//So, when we try to convert into boolean
+//1 => true
+//0=> false
+//"burhan"=>true
+//null=> false
+//undefined=> false
+//""=> false
+// Now let's convert into string
+let number = undefined;
+let stringInNumber = String(number)
+console.log(typeof (stringInNumber))
+console.log(stringInNumber)
+
+
