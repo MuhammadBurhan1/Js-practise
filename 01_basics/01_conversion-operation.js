8ea275696d9f6cc0f183ef1fa86bc19965603202
MuhammadBurhan1 <128923339+MuhammadBurhan1@users.noreply.github.com>
2025-06-01 10:00:15 +0000

    updated

--- 01_basics/01_conversion-operation.js
+++ 01_basics/01_conversion-operation.js
@@ -26,9 +26,44 @@
 //undefined=> false
 //""=> false
 // Now let's convert into string
-let number = undefined;
-let stringInNumber = String(number)
-console.log(typeof (stringInNumber))
-console.log(stringInNumber)
+// let number = undefined;
+// let stringInNumber = String(number)
+// console.log(typeof (stringInNumber))
+// console.log(stringInNumber)
+// let number = undefined;
+// let stringInNumber = String(number)
+// console.log(typeof (stringInNumber))
+// console.log(stringInNumber)
+//********** Operations *************************************************
+let value = 3
+let negValue = -value
+console.log(negValue)
+console.log(2+2)
+console.log(2-2)
+console.log(2/2)
+console.log(2%3)
+console.log(2**3)
+let str1 = "Assalam mu Allikum"
+let str2 = " Burhan"
+let str3 = str1 + str2 
+console.log(str3)
+console.log(1 + "2") // 12
+console.log("1"+ 2)  // 12
+console.log("1"+ "2")// 12
+console.log("1"+"2"+2)//122
+console.log("1"+2+2) //122
+console.log(1+2+"2")// 3+"2"=>32
+console.log(+true);//1
+console.log(+false);//0
+let gameCounter = 100
+++gameCounter
+//gameCounter++ //Postfix operator 
+console.log(gameCounter)
+let a= 5;
+let b= a++; //Postfix operator
+console.log(`${a},${b}`)
 
+let x =2 ;
+let y=++x; // Prefix Operator
+console.log(`${x},${y}`);
 
