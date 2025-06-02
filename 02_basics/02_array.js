4490a1bc88322958f7cc9ac6c7359eff953c18ee
MuhammadBurhan1 <128923339+MuhammadBurhan1@users.noreply.github.com>
2025-06-02 09:45:48 +0000

    Done

---
+++ 02_basics/02_array.js
@@ -0,0 +1,29 @@
+const myArr1 = [1,2,3,4,5];
+// console.log(myArr1);
+const myArr2 = new Array("Burhan, Ahmad")
+// console.log(myArr2)
+// console.log(myArr1[1]);
+// Array Methods
+// Push Method adds the value to the array
+// myArr1.push(6);
+// myArr1.push(7);
+// myArr1.pop();
+// myArr1.unshift(9)
+// myArr1.shift();
+// console.log(myArr1.includes(9));
+// console.log(myArr1.indexOf(3));
+const newArr = myArr1.join();
+// console.log(myArr1);
+// console.log(newArr);
+// console.log(typeof newArr)
+
+
+console.log("A", myArr1);
+const newOne = myArr1.slice(1,3)
+console.log(myArr1)
+console.log("B",newOne);
+
+const newTwo = myArr1.splice(1,3)
+console.log(myArr1);
+console.log("C",newTwo);
+
