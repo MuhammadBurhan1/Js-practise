fb4ed8ecb14f3c6ee85e53b529159d416a7d057e
MuhammadBurhan1 <128923339+MuhammadBurhan1@users.noreply.github.com>
2025-06-22 06:40:08 +0000

    done

--- 03_basics/03_scope.js
+++ 03_basics/03_scope.js
@@ -6,6 +6,51 @@ let a =10
 const b = 20
 console.log("INNER", a)
 }
-console.log(a)
+// console.log(a)
 // console.log(b)
+const cricket= {
+    name: "cricket"
+}
+const {name:cric}= cricket
+// console.log(cric)
+function one (){
+    const username = "Muhammad Burhan"
+    // console.log(username)
+    function two (){
+        const website = "youtube"
+        console.log(username)
+        // console.log(website)
+    }
+    // console.log(website)
+    two()
+}
+one()
+const obj1 ={1:"a",2:"b"}
+const obj2 = {3:"c",4:"d"}
+const objassign=Object.assign({},obj1,obj2)
+// console.log(objassign)
+if (true){
+    const username = "MuhammadBurhan"
+    if(username==="MuhammadBurhan"){
+        const website ="youtube"
+        console.log(username);
+        console.log(website)
+    }
+    // console.log(website)
+}
+// console.log(username)
 
+
+
+
+// +++++++++++++++++++++++++++ Interesting +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const resultOne= addOne(5)
+console.log(resultOne)
+function addOne(num){
+return num + 1
+}
+const addTwo = function (num){          // Function expression
+    return num + 2
+}
+const resultTwo=addTwo(5)
+console.log(resultTwo)
