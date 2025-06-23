c792d459d72c9dd22ea53cba58f35cf9351c4b61
MuhammadBurhan1 <128923339+MuhammadBurhan1@users.noreply.github.com>
2025-06-23 11:02:11 +0000

    done

---
+++ 05_iterations/whileanddowhileloop.js
@@ -0,0 +1,21 @@
+"use strict "; // Treat all JS code as newer version
+let index = 0 ;
+while(index < 10){
+    console.log(` so, the index is ${index}`);
+    index = index + 2;
+
+}
+let myArray=["ahmad","burhan", "abdul"];
+let i = 0 ;
+while(i<myArray.length){
+    const element = myArray[i];
+    // console.log(element)
+    i++ ;
+
+
+}
+let score = 11
+do{
+console.log(`the score is ${score}`)
+score++
+}while(score<=10)
